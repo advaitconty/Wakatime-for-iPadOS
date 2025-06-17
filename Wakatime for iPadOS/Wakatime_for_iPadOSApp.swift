f5ae425ae7db363d34f460f638c257bfc2253246
contyadvait <advaitthepro@outlook.com>
2025-06-17 12:37:36 +0800

    Initial Commit

---
+++ Wakatime for iPadOS/Wakatime_for_iPadOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Wakatime_for_iPadOSApp.swift
+//  Wakatime for iPadOS
+//
+//  Created by Milind Contractor on 17/6/25.
+//
+
+import SwiftUI
+
+@main
+struct Wakatime_for_iPadOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
